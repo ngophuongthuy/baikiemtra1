e77a8412d890cf1b59e3bfebe0ba9f3098cafa71
ngophuongthuy <91355514+ngophuongthuy@users.noreply.github.com>
2021-09-25 11:01:20 +0700

    Add files via upload

---
+++ bai1.py
@@ -0,0 +1,19 @@
+a = [1, 2, 6, 8, 9, 4, 5, 6, 45, 34, 66, 44, 37, 78]
+b = []
+for i in a:
+    if(i < 30):
+        b.append(i)
+        print(b)
+
+c = int(input("Nhap 1 so: "))
+d = []
+for i in a:
+    if i < c:
+        d.append(i)
+        print(d)
+e = int(input("Nhap 1 so: "))
+g = []
+for i in a:
+    if i > e:
+        g.append(i)
+        print(g)
